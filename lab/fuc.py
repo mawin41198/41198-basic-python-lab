c5f9323f064931368061f2e8d09fb597f256e0c8
mawin41198 <41198@virtual.prc.ac.th>
2025-08-26 22:17:54 +0700

    while loop

---
+++ lab/fuc.py
@@ -0,0 +1,7 @@
+def cal(number1):
+    #number1 = 20
+    number2 = 30
+    print(number1+number2)
+
+getNumber = int(input("enter your number"))
+cal(getNumber)
---
+++ work/Lab08_While_Functions/2.py
@@ -0,0 +1,11 @@
+total = 0
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    total += n
+print(total)
+
+
+
+
